64e467b1098e3445651ca1181d234564cb13d9ef
Jonatan Goulart <jonatan784@gmail.com>
2023-03-03 00:08:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/api-github-users/'
+  base: '/api-github-users/',
   plugins: [react()],
   resolve: {
     alias: {
